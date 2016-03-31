295f542ed734d80a0595a1887274dc47f712941d
Chris Boden <cboden@gmail.com>
2016-03-31 12:37:44 -0400

    -V now vhost (alignment with rabbitmqadmin)
    Updated command descriptions, more helpful

--- src/Application.php
+++ src/Application.php
@@ -1,7 +1,10 @@
 <?php
 namespace Binky;
 use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
 class Application extends SymfonyApplication {
     private $cmd;
@@ -29,5 +32,20 @@ class Application extends SymfonyApplication {
         $inputDefinition->setArguments();
 
         return $inputDefinition;
+    }
+
+    protected function getDefaultInputDefinition()
+    {
+        return new InputDefinition(array(
+            new InputArgument('command', InputArgument::REQUIRED, 'The command to execute'),
+
+            new InputOption('--help', '-h', InputOption::VALUE_NONE, 'Display this help message'),
+            new InputOption('--quiet', '-q', InputOption::VALUE_NONE, 'Do not output any message'),
+            new InputOption('--verbose', '-v|vv|vvv', InputOption::VALUE_NONE, 'Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug'),
+            new InputOption('--version', '', InputOption::VALUE_NONE, 'Display this application version'),
+            new InputOption('--ansi', '', InputOption::VALUE_NONE, 'Force ANSI output'),
+            new InputOption('--no-ansi', '', InputOption::VALUE_NONE, 'Disable ANSI output'),
+            new InputOption('--no-interaction', '-n', InputOption::VALUE_NONE, 'Do not ask any interactive question'),
+        ));
     }
 }
